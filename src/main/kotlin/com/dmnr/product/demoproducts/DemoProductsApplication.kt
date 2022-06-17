56c07547649a80faf31f9b82c470a1d306505c33
Raul Demner <rauldemner.lopez@globant.com>
2022-06-17 15:19:13 -0500

    refactoring

--- src/main/kotlin/com/dmnr/product/demoproducts/DemoProductsApplication.kt
+++ src/main/kotlin/com/dmnr/product/demoproducts/DemoProductsApplication.kt
@@ -2,10 +2,15 @@ package com.dmnr.product.demoproducts
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
 
 @SpringBootApplication
 class DemoProductsApplication
 
+@Configuration
+@ComponentScan("com.dmnr.product.demoproducts")
+class DemoConfiguration
 fun main(args: Array<String>) {
     runApplication<DemoProductsApplication>(*args)
 }
--- src/main/kotlin/com/dmnr/product/demoproducts/repository/MessageRepository.kt
+++ src/main/kotlin/com/dmnr/product/demoproducts/repository/MessageRepository.kt
@@ -3,7 +3,10 @@ package com.dmnr.product.demoproducts.repository
 import com.dmnr.product.demoproducts.entity.Message
 import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
 
+
+@Repository
 interface MessageRepository:CrudRepository<Message,String> {
 
     @Query("select from messages")
